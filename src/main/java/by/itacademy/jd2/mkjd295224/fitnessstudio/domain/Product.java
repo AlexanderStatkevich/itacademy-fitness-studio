7801c7751adbb0c21871cd59f7a22f93652fd4ac
bumblebear <aastatkevich@gmail.com>
2023-02-24 13:42:42 +0300

    base entity class added

--- src/main/java/by/itacademy/jd2/mkjd295224/fitnessstudio/domain/Product.java
+++ src/main/java/by/itacademy/jd2/mkjd295224/fitnessstudio/domain/Product.java
@@ -1,18 +1,9 @@
 package by.itacademy.jd2.mkjd295224.fitnessstudio.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
 @Entity
-public class Product {
-    private Long id;
+public class Product extends BaseEntity {
 
-    @Id
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
--- src/main/java/by/itacademy/jd2/mkjd295224/fitnessstudio/domain/Recipe.java
+++ src/main/java/by/itacademy/jd2/mkjd295224/fitnessstudio/domain/Recipe.java
@@ -1,18 +1,7 @@
 package by.itacademy.jd2.mkjd295224.fitnessstudio.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
 @Entity
-public class Recipe {
-    private Long id;
-
-    @Id
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+public class Recipe extends BaseEntity {
 }
